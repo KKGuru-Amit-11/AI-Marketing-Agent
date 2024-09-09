857b9fdd5a886c92ce09b87222f2311537d76f75
Amit Kumar <142104891+KKGuru-Amit-11@users.noreply.github.com>
2024-09-09 14:00:34 +0530

    tools.py

--- tools.py
+++ tools.py
@@ -1,9 +1,7 @@
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 import os
-
-os.environ['SERPER_API_KEY'] = os.getenv('SERPER_API_KEY')
-
+os.environ['SERPER_API_KEY']='5e91bacd42a33cdf4299197ce6d7e49aaca23310'
 
 from crewai_tools import SerperDevTool
 
